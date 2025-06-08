fe2d8cc8c96a8709cf20782b7c6620bdfcb2ce13
michaelBezick <mbezick@purdue.edu>
2025-06-08 11:14:06 -0400

    plots

--- plotting/better_bootstrapping.py
+++ plotting/better_bootstrapping.py
@@ -2,9 +2,9 @@ from functions2 import *
 
 # ‣ “one group, many metrics”  (train vs eval case)
 plot_curves(
-    group_names = ["pca_test_ortho_mismatch_forreal", "10_0_0mm_pca", "30_0_30mm_pca"],
-    y_keys      = ["rollout/Success"],
-    y_labels    = ["Train"],
+    group_names = ["correctly disambiguated PCA 1 camera train eval same position but world vs camera coords"],
+    y_keys      = ["rollout/Success", "eval/Success"],
+    y_labels    = ["Train", "Eval"],
     key_x       = "_step",
-    filename    = "pca_test_train_vs_eval_success.pdf"
+    filename    = "disambiguated_pca_test_train_vs_eval_success.pdf"
 )
