459818ca0651b891ef2b92ba7343e8284eebbb2c
Michael Bezick <mbezick@purdue.edu>
2026-02-05 10:39:43 -0500

    Changed to abspath for gilbreth scripts.

--- gilbreth_scripts/turn_faucet_dr.sh
+++ gilbreth_scripts/turn_faucet_dr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch --export=GROUP_NAME=DR_TF_final,CONF_DIR=../slurm_confs/turn_faucet_dr,PPRL_ENV=turn_faucet run_6seeds.sbatch
+sbatch --export=GROUP_NAME=DR_TF_final,CONF_DIR=/home/mbezick/Repos/pprlPCA/slurm_confs/turn_faucet_dr,PPRL_ENV=turn_faucet run_6seeds.sbatch
--- gilbreth_scripts/turn_faucet_pca.sh
+++ gilbreth_scripts/turn_faucet_pca.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch --export=GROUP_NAME=PCA_TF_final,CONF_DIR=../slurm_confs/turn_faucet_pca,PPRL_ENV=turn_faucet run_6seeds.sbatch
+sbatch --export=GROUP_NAME=PCA_TF_no_extra,CONF_DIR=/home/mbezick/Repos/pprlPCA/slurm_confs/turn_faucet_pca,PPRL_ENV=turn_faucet run_6seeds.sbatch
