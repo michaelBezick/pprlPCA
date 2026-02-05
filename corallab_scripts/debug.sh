18d245547aca5b94db5c45d2453e47c5f39797d5
michaelBezick <mbezick@purdue.edu>
2026-02-05 10:21:21 -0500

    Added corallab scripts directory.

---
+++ corallab_scripts/debug.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+ulimit -n 4096
+HYDRA_FULL_ERROR=1 CUDA_VISIBLE_DEVICES=1 python ./scripts/maniskill_train_sac2.py -cp "../slurm_confs/turn_faucet_pca" parallel=False env=turn_faucet model=ppt platform=debug batch_B=1 runner.n_steps=1 eval.n_eval_envs=1 eval.max_trajectories=1 runner.log_interval_steps=1
---
+++ corallab_scripts/turn_faucet_baseline.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
+nohup env CUDA_VISIBLE_DEVICES=0 python ./scripts/maniskill_train_sac2.py \
+  -cp "../slurm_confs/turn_faucet_baseline" env=turn_faucet model=pointgpt_rl algo=aux_sac \
+  > train.log 2>&1 &
+disown
