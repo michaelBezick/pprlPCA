3dc10d367887f61706d911369b2c36df18ab79b9
michaelBezick <mbezick@purdue.edu>
2026-02-16 17:27:36 -0500

    Add submit all file

---
+++ submit_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+bash ./gilbreth_scripts/push_chair_pca.sh
+bash ./gilbreth_scripts/push_chair_baseline.sh
+
+bash ./gilbreth_scripts/turn_faucet_pca.sh
+bash ./gilbreth_scripts/turn_faucet_baseline.sh
+
+bash ./gilbreth_scripts/open_cabinet_pca.sh
+bash ./gilbreth_scripts/open_cabinet_baseline.sh
