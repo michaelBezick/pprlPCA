db8b4a858cf2f867d3b867829a6b41427a9e2d88
Michael Bezick <mbezick@purdue.edu>
2026-02-16 17:21:28 -0500

    Add and fix gilbreth jobs

--- gilbreth_scripts/open_cabinet_pca.sh
+++ gilbreth_scripts/open_cabinet_pca.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch --export=GROUP_NAME=PCA_OCD_no_extra,CONF_DIR=../slurm_confs/open_cabinet_drawer_pca,PPRL_ENV=open_cabinet_drawer /home/mbezick/Repos/pprlPCA/gilbreth_scripts/run_6seeds.sbatch
+sbatch --export=GROUP_NAME=OCD_PCA,CONF_DIR=../slurm_confs/open_cabinet_drawer_pca,PPRL_ENV=open_cabinet_drawer /home/mbezick/Repos/pprlPCA/gilbreth_scripts/run_6seeds.sbatch
--- gilbreth_scripts/turn_faucet_baseline.sh
+++ gilbreth_scripts/turn_faucet_baseline.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbatch --export=GROUP_NAME=baseline_TF_no_extra,CONF_DIR=/home/mbezick/Repos/pprlPCA/slurm_confs/turn_faucet_baseline,PPRL_ENV=turn_faucet /home/mbezick/Repos/pprlPCA/gilbreth_scripts/run_6seeds.sbatch
+sbatch --export=GROUP_NAME=TF_baseline,CONF_DIR=/home/mbezick/Repos/pprlPCA/slurm_confs/turn_faucet_baseline,PPRL_ENV=turn_faucet /home/mbezick/Repos/pprlPCA/gilbreth_scripts/run_6seeds.sbatch
