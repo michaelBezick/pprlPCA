276fcf5824cb1c1400fb22ca9411a14b250ccc2b
michaelBezick <mbezick@purdue.edu>
2026-02-17 07:29:22 -0500

    Add group name to corallab_scripts

--- corallab_scripts/push_chair_baseline.sh
+++ corallab_scripts/push_chair_baseline.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
-nohup env CUDA_VISIBLE_DEVICES=0 python ./scripts/train_sac.py \
+nohup env GROUP_NAME=PC_Baseline CUDA_VISIBLE_DEVICES=2 python ./scripts/train_sac.py \
   -cp "../slurm_confs/push_chair_baseline" env=push_chair model=pointgpt_rl algo=aux_sac \
   > train.log 2>&1 &
 disown
--- corallab_scripts/turn_faucet_baseline.sh
+++ corallab_scripts/turn_faucet_baseline.sh
@@ -2,7 +2,7 @@
 
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
-nohup env CUDA_VISIBLE_DEVICES=1 python ./scripts/train_sac.py \
+nohup env GROUP_NAME=PC_PCA CUDA_VISIBLE_DEVICES=1 python ./scripts/train_sac.py \
   -cp "../slurm_confs/turn_faucet_baseline" env=turn_faucet model=pointgpt_rl algo=aux_sac \
   > train.log 2>&1 &
 disown
