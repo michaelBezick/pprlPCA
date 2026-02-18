134092fa7c97c90350d25a403fc21ef1d3e2d765
michaelBezick <mbezick@purdue.edu>
2026-02-18 11:14:19 -0500

    Fix debug script group name

--- corallab_scripts/debug.sh
+++ corallab_scripts/debug.sh
@@ -2,4 +2,4 @@
 
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 ulimit -n 4096
-HYDRA_FULL_ERROR=1 CUDA_VISIBLE_DEVICES=0 python ./scripts/train_sac.py -cp "../slurm_confs/turn_faucet_pca" parallel=False env=turn_faucet model=ppt platform=debug batch_B=1 eval.n_eval_envs=1 eval.max_trajectories=1 runner.log_interval_steps=1
+HYDRA_FULL_ERROR=1 CUDA_VISIBLE_DEVICES=0 GROUP_NAME=DEBUG python ./scripts/train_sac.py -cp "../slurm_confs/open_cabinet_door_baseline" parallel=False env=open_cabinet_door model=ppt platform=debug batch_B=1 eval.n_eval_envs=1 eval.max_trajectories=1 runner.log_interval_steps=1 wandb.group_name=DEBUG
